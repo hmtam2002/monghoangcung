f1ca2bb209b8aa85a079b13576b597a8486758cd
Minh_Tam <0306201285@caothang.edu.vn>
2022-11-11 15:28:39 +0700

    pushmoinhat

--- lib/screens/dangky/dienthongtin.dart
+++ lib/screens/dangky/dienthongtin.dart
@@ -1,116 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:monghoangcung/screens/dangnhap/dangnhapthanhcong.dart';
 
 class dienthongtin extends StatelessWidget {
   const dienthongtin({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-        image: AssetImage('assets/a.png'),
-        fit: BoxFit.cover,
-      )),
-      child: Container(
-        padding: const EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          color: Colors.white.withOpacity(0.8),
-          borderRadius: BorderRadius.circular(50),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              'Nhập thông tin',
-              style: TextStyle(
-                fontSize: 30,
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/a.png'),
+          fit: BoxFit.cover,
+        )),
+        child: Container(
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.8),
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Nhập thông tin',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
               ),
-            ),
-            const TextField(
-              style: TextStyle(
-                color: Colors.grey,
-              ),
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  // borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    width: 3,
+              const TextField(
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    // borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  border: OutlineInputBorder(),
+                  hintText: "Tên tài khoản",
+                  hintStyle: TextStyle(
                     color: Colors.grey,
                   ),
                 ),
-                border: OutlineInputBorder(),
-                hintText: "Tên tài khoản",
-                hintStyle: TextStyle(
+              ),
+              const TextField(
+                style: TextStyle(
                   color: Colors.grey,
                 ),
-              ),
-            ),
-            const TextField(
-              style: TextStyle(
-                color: Colors.grey,
-              ),
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  // borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    width: 3,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    // borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  border: OutlineInputBorder(),
+                  hintText: "Email",
+                  hintStyle: TextStyle(
                     color: Colors.grey,
                   ),
                 ),
-                border: OutlineInputBorder(),
-                hintText: "Email",
-                hintStyle: TextStyle(
+              ),
+              const TextField(
+                style: TextStyle(
                   color: Colors.grey,
                 ),
-              ),
-            ),
-            const TextField(
-              style: TextStyle(
-                color: Colors.grey,
-              ),
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  // borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    width: 3,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    // borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  border: OutlineInputBorder(),
+                  hintText: "Ngày sinh",
+                  hintStyle: TextStyle(
                     color: Colors.grey,
                   ),
                 ),
-                border: OutlineInputBorder(),
-                hintText: "Ngày sinh",
-                hintStyle: TextStyle(
+              ),
+              const TextField(
+                style: TextStyle(
                   color: Colors.grey,
                 ),
-              ),
-            ),
-            const TextField(
-              style: TextStyle(
-                color: Colors.grey,
-              ),
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  // borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    width: 3,
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    // borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      width: 3,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  border: OutlineInputBorder(),
+                  hintText: "Số điện thoại",
+                  hintStyle: TextStyle(
                     color: Colors.grey,
                   ),
                 ),
-                border: OutlineInputBorder(),
-                hintText: "Số điện thoại",
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Đăng ký'),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const dangnhapthanhcong()));
+                },
+                child: const Text('Đăng ký'),
+              ),
+            ],
+          ),
         ),
       ),
     );
