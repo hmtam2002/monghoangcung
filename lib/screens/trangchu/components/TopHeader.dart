174cfbf4086d51edc30414856ac3dd3f873ee2be
Tuong_Vy <0306201305@caothang.edu.vn>
2022-11-12 22:18:11 +0700

    tack components

---
+++ lib/screens/trangchu/components/TopHeader.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TopHeader extends StatelessWidget {
+  const TopHeader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        TextButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const trangcanhan()));
+          },
+          child: Icon(
+            Icons.account_circle_rounded,
+            color: Colors.brown.withOpacity(0.8),
+            size: 50,
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const trangcanhan()));
+          },
+          child: Icon(
+            Icons.menu,
+            color: Colors.brown.withOpacity(0.8),
+            size: 50,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/trangchu/trangchu.dart
+++ lib/screens/trangchu/trangchu.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:monghoangcung/screens/chonlevel/chonlevel.dart';
 import 'package:monghoangcung/screens/trangcanhan/trangcanhan.dart';
 
+import 'components/TopHeader.dart';
+
 class trangchu extends StatefulWidget {
   const trangchu({super.key});
 
@@ -12,107 +14,81 @@ class trangchu extends StatefulWidget {
 class _trangchuState extends State<trangchu> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-        image: AssetImage('assets/a.png'),
-        fit: BoxFit.cover,
-      )),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const trangcanhan()));
-                },
-                child: Icon(
-                  Icons.account_circle_rounded,
-                  color: Colors.brown.withOpacity(0.8),
-                  size: 50,
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const trangcanhan()));
-                },
-                child: Icon(
-                  Icons.menu,
-                  color: Colors.brown.withOpacity(0.8),
-                  size: 50,
-                ),
-              ),
-            ],
-          ),
-          const Image(
-            image: AssetImage('assets/Logo.png'),
-          ),
-          Column(
-            children: [
-              Container(
-                height: 50,
-                width: 200,
-                margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const Chooselv()));
-                  },
-                  child: const Text(
-                    'Chơi đơn',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87),
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/a.png'),
+          fit: BoxFit.cover,
+        )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const TopHeader(),
+            const Image(
+              image: AssetImage('assets/Logo.png'),
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 50,
+                  width: 200,
+                  margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Chooselv()));
+                    },
+                    child: const Text(
+                      'Chơi đơn',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87),
+                    ),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.white.withOpacity(0.65)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
                   ),
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Colors.white.withOpacity(0.65)),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                      )),
                 ),
-              ),
-              Container(
-                height: 50,
-                width: 200,
-                margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Chơi đối kháng',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87),
+                Container(
+                  height: 50,
+                  width: 200,
+                  margin: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Chơi đối kháng',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87),
+                    ),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.white.withOpacity(0.5)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                        )),
                   ),
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Colors.white.withOpacity(0.5)),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                      )),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
