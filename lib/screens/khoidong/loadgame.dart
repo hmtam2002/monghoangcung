2a4a419f76d15b8e7e58c132d5b2c99d565404c6
tuongvycute <0306201305@caothang.edu.vn>
2022-11-13 15:15:00 +0700

    chỉnh đôi chút

--- lib/screens/khoidong/loadgame.dart
+++ lib/screens/khoidong/loadgame.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:monghoangcung/components/nen_game.dart';
 
 class loadgame extends StatelessWidget {
   const loadgame({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-        image: AssetImage('assets/a.png'),
-        fit: BoxFit.cover,
-      )),
+    return nen_game(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
