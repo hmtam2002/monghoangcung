11f93dbad2ea4e085c80153a652d4963266ec3f5
tuongvycute <0306201305@caothang.edu.vn>
2022-11-26 08:09:31 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:monghoangcung/screens/dangky/4_thanhcong.dart';
 
-Future<void> main(List<String> agrs) async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+//khởi tạo cho firebase chạy 1 lần
+// Future<void> main(List<String> agrs) async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
+
+void main() {
   runApp(const MyApp());
 }
 
