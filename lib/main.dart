f1656481ef05e3e22a65cd418711e19431ed1985
tuongvycute <0306201305@caothang.edu.vn>
2022-11-30 13:04:31 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:monghoangcung/screens/dangky/4_thanhcong.dart';
 import 'package:monghoangcung/screens/trangcanhan/trangcanhan.dart';
-
+import 'package:flutter/services.dart';
+import 'package:monghoangcung/screens/trangchu/trangchu.dart';
 //khởi tạo cho firebase chạy 1 lần
 // Future<void> main(List<String> agrs) async {
 //   WidgetsFlutterBinding.ensureInitialized();
@@ -18,13 +17,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const trangcanhan(),
+      home: const trangchu(),
     );
   }
 }
