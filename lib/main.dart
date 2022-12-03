bdd6be6f1945e4818a43b5c0b72e1bd6ae9297ca
tuongvycute <0306201305@caothang.edu.vn>
2022-12-04 00:27:35 +0700

    gì đó

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:monghoangcung/screens/trangchu/trangchu.dart';
 //khởi tạo cho firebase chạy 1 lần
 // Future<void> main(List<String> agrs) async {
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
--- lib/screens/trangcanhan/trangcanhan.dart
+++ lib/screens/trangcanhan/trangcanhan.dart
@@ -24,103 +24,107 @@ class _trangcanhanState extends State<trangcanhan> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        //ảnh nền
-        padding: const EdgeInsets.only(
-          top: kDefaultPadding,
-          left: kDefaultPadding,
-          right: kDefaultPadding,
-        ),
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage('assets/a.png'),
-          fit: BoxFit.cover,
-        )),
-        child: Column(
-          //nội dung
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: ListView(
+        children: [
+          Container(
+            //ảnh nền
+            padding: const EdgeInsets.only(
+              top: kDefaultPadding,
+              left: kDefaultPadding,
+              right: kDefaultPadding,
+            ),
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/a.png'),
+              fit: BoxFit.cover,
+            )),
+            child: Column(
+              //nội dung
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const trangchu(),
-                        ));
-                  },
-                  child: Icon(
-                    Icons.home_rounded,
-                    color: Colors.brown.withOpacity(0.8),
-                    size: 50,
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const trangchu(),
+                            ));
+                      },
+                      child: Icon(
+                        Icons.home_rounded,
+                        color: Colors.brown.withOpacity(0.8),
+                        size: 50,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        Icons.clear,
+                        color: Colors.brown.withOpacity(0.8),
+                        size: 50,
+                      ),
+                    ),
+                  ],
                 ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Icon(
-                    Icons.clear,
-                    color: Colors.brown.withOpacity(0.8),
-                    size: 50,
+                Container(
+                  child: Column(
+                    children: [
+                      const gioithieuthongtin(),
+                      cacnuttrongtrangcanhan(
+                        noidung: 'Chỉnh sửa thông tin',
+                        press: MaterialPageRoute(
+                            builder: (context) => const EditInfo()),
+                      ),
+                      cacnuttrongtrangcanhan(
+                        noidung: 'Xem xếp hạng',
+                        press: MaterialPageRoute(
+                            builder: (context) => const xemxephang()),
+                      ),
+                      cacnuttrongtrangcanhan(
+                        noidung: 'Trợ giúp',
+                        press: MaterialPageRoute(
+                            builder: (context) => const trogiup()),
+                      ),
+                      cacnuttrongtrangcanhan(
+                        noidung: 'Nhận thư',
+                        press: MaterialPageRoute(
+                            builder: (context) => const nhan_thu()),
+                      ),
+                      cacnuttrongtrangcanhan(
+                        noidung: 'Nạp tiền',
+                        press: MaterialPageRoute(
+                            builder: (context) => const naptien()),
+                      ),
+                      cacnuttrongtrangcanhan(
+                        noidung: 'Cài đặt',
+                        press: MaterialPageRoute(
+                            builder: (context) => const cai_dat()),
+                      ),
+                      cacnuttrongtrangcanhan(
+                        noidung: 'Đăng ký',
+                        press: MaterialPageRoute(
+                            builder: (context) => const nhapusername()),
+                      ),
+                      cacnuttrongtrangcanhan(
+                        noidung: 'Đăng Nhập',
+                        press: MaterialPageRoute(
+                            builder: (context) => const nhapmatkhau()),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-            Container(
-              child: Column(
-                children: [
-                  const gioithieuthongtin(),
-                  cacnuttrongtrangcanhan(
-                    noidung: 'Chỉnh sửa thông tin',
-                    press: MaterialPageRoute(
-                        builder: (context) => const EditInfo()),
-                  ),
-                  cacnuttrongtrangcanhan(
-                    noidung: 'Xem xếp hạng',
-                    press: MaterialPageRoute(
-                        builder: (context) => const xemxephang()),
-                  ),
-                  cacnuttrongtrangcanhan(
-                    noidung: 'Trợ giúp',
-                    press: MaterialPageRoute(
-                        builder: (context) => const trogiup()),
-                  ),
-                  cacnuttrongtrangcanhan(
-                    noidung: 'Nhận thư',
-                    press: MaterialPageRoute(
-                        builder: (context) => const nhan_thu()),
-                  ),
-                  cacnuttrongtrangcanhan(
-                    noidung: 'Nạp tiền',
-                    press: MaterialPageRoute(
-                        builder: (context) => const naptien()),
-                  ),
-                  cacnuttrongtrangcanhan(
-                    noidung: 'Cài đặt',
-                    press: MaterialPageRoute(
-                        builder: (context) => const cai_dat()),
-                  ),
-                  cacnuttrongtrangcanhan(
-                    noidung: 'Đăng ký',
-                    press: MaterialPageRoute(
-                        builder: (context) => const nhapusername()),
-                  ),
-                  cacnuttrongtrangcanhan(
-                    noidung: 'Đăng Nhập',
-                    press: MaterialPageRoute(
-                        builder: (context) => const nhapmatkhau()),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
     ;
