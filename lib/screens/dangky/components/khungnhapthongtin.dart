da8a468a72bcf52da066d68d04f3da45987218a4
maihuuphuoc2704 <113123291+maihuuphuoc2704@users.noreply.github.com>
2022-12-03 14:23:32 +0700

    a
    a

--- lib/screens/dangky/components/khungnhapthongtin.dart
+++ lib/screens/dangky/components/khungnhapthongtin.dart
@@ -2,22 +2,50 @@ import 'package:flutter/material.dart';
 
 import '../nhapusername/components/nhapthongtinbatdau.dart';
 
-class khungnhapthongtin extends StatelessWidget {
-  const khungnhapthongtin({
+class khungnhapthongtin extends StatefulWidget {
+  khungnhapthongtin({
     Key? key,
     required this.text,
     this.icon = Icons.person,
   }) : super(key: key);
   final String text;
   final IconData icon;
+
+  @override
+  State<khungnhapthongtin> createState() => _khungnhapthongtinState();
+}
+
+class _khungnhapthongtinState extends State<khungnhapthongtin> {
+  bool isHiden = true;
+
+  Icon iconShow = const Icon(Icons.visibility_off);
+
   @override
   Widget build(BuildContext context) {
     return nhapthongtin_bandau(
       child: TextField(
+        obscureText: isHiden,
         decoration: InputDecoration(
-          icon: Icon(icon),
-          hintText: text,
-          border: InputBorder.none,
+          border: OutlineInputBorder(),
+          prefixIcon: Icon(Icons.password),
+          suffixIcon: GestureDetector(
+            onTap: () {
+              setState(() {
+                isHiden = !isHiden;
+              });
+
+              if (isHiden) {
+                setState(() {
+                  iconShow = const Icon(Icons.visibility_off);
+                });
+              } else {
+                setState(() {
+                  iconShow = const Icon(Icons.visibility);
+                });
+              }
+            },
+            child: iconShow,
+          ),
         ),
       ),
     );
