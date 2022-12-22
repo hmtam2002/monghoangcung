0f4af1e570cf8042e809f02cf8fe89bb1dec462a
tuongvycute <0306201285@caothang.edu.vn>
2022-12-22 11:51:18 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:monghoangcung/screens/dangky/components/dangky.dart';
-
-import 'package:monghoangcung/screens/dangky/components/khungnhapthongtin.dart';
-
 import 'package:monghoangcung/screens/trangdautien/trangdautien.dart';
-import 'package:monghoangcung/test.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,7 +15,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
