20188923847590aa211c0e7335d3b36751f2db5a
tuongvycute <0306201285@caothang.edu.vn>
2022-12-26 18:46:59 +0700

    Update TopHeader.dart

--- lib/screens/trangchu/components/TopHeader.dart
+++ lib/screens/trangchu/components/TopHeader.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import '../../../object/Accounts.dart';
 import '../../trangcanhan/trangcanhan.dart';
 
 class TopHeader extends StatefulWidget {
@@ -9,31 +12,55 @@ class TopHeader extends StatefulWidget {
 }
 
 class _TopHeaderState extends State<TopHeader> {
+  final accid = FirebaseAuth.instance.currentUser?.uid;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        TextButton(
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const TrangCaNhan()));
-            },
-            child: const CircleAvatar(
-              backgroundImage: AssetImage('assets/1.jpg'),
-            )),
-        // TextButton(
-        //   onPressed: () {
-        //     Navigator.push(context,
-        //         MaterialPageRoute(builder: (context) => const TrangCaNhan()));
-        //   },
-        //   child: Icon(
-        //     Icons.menu_rounded,
-        //     color: Colors.brown.withOpacity(0.8),
-        //     size: 50,
-        //   ),
-        // ),
-      ],
-    );
+    return FutureBuilder(
+        future: readAccount(),
+        builder: ((context, snapshot) {
+          if (snapshot.hasData) {
+            final account = snapshot.data;
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const TrangCaNhan()));
+                    },
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage(account!.picture),
+                    )),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const TrangCaNhan()));
+                  },
+                  child: Icon(
+                    Icons.menu_rounded,
+                    color: Colors.brown.withOpacity(0.8),
+                    size: 50,
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Container();
+          }
+        }));
+  }
+
+  Future<Account?> readAccount() async {
+    final docAccounts =
+        FirebaseFirestore.instance.collection('accounts').doc(accid);
+    final snapshot = await docAccounts.get();
+    if (snapshot.exists) {
+      return Account.fromJson(snapshot.data()!);
+    }
+    return null;
   }
 }
