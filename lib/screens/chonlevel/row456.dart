a542ac46397375ab65ad467e8509aa7620b580eb
maihuuphuoc2704 <113123291+maihuuphuoc2704@users.noreply.github.com>
2022-12-28 23:02:55 +0700

    Update row456.dart

--- lib/screens/chonlevel/row456.dart
+++ lib/screens/chonlevel/row456.dart
@@ -21,140 +21,131 @@ class _Row456State extends State<Row456> {
   bool i1 = false;
 
   bool i2 = false;
-  final accid = FirebaseAuth.instance.currentUser?.uid;
-  Future<AccountObject?> readAccount() async {
-    final docAccounts =
-        FirebaseFirestore.instance.collection('accounts').doc(accid);
-    final snapshot = await docAccounts.get();
-    if (snapshot.exists) {
-      return AccountObject.fromJson(snapshot.data()!);
-    }
-    return null;
+  bool i3 = false;
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      if (widget.lv >= 4) {
+        i1 = true;
+        color1 = Colors.white.withOpacity(0.8);
+      }
+      if (widget.lv >= 5) {
+        i2 = true;
+        color2 = Colors.white.withOpacity(0.8);
+      }
+      if (widget.lv >= 6) {
+        i3 = true;
+        color3 = Colors.white.withOpacity(0.8);
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: readAccount(),
-      builder: ((context, snapshot) {
-        if (snapshot.hasData) {
-          final account = snapshot.data;
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 70,
-                width: 70,
-                margin: const EdgeInsets.all(20),
-                child: ElevatedButton(
-                  onPressed: (account!.lv >= 4)
-                      ? () {
-                          setState(() {
-                            color1 = Colors.white.withOpacity(0.8);
-                          });
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ManHinhTraLoi(
-                                id: 31,
-                                point: 0,
-                                soluongcau: 1,
-                              ),
-                            ),
-                          );
-                        }
-                      : null,
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(color1),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                      )),
-                  child: const Text(
-                    '4',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87),
-                  ),
-                ),
-              ),
-              Container(
-                height: 70,
-                width: 70,
-                margin: const EdgeInsets.all(20),
-                child: ElevatedButton(
-                  onPressed: (account.lv >= 5)
-                      ? () {
-                          setState(() {
-                            color2 = Colors.white.withOpacity(0.8);
-                          });
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ManHinhTraLoi(
-                                        id: 41,
-                                        point: 0,
-                                        soluongcau: 1,
-                                      )));
-                        }
-                      : null,
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(color2),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                      )),
-                  child: const Text(
-                    '5',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87),
-                  ),
-                ),
-              ),
-              Container(
-                height: 70,
-                width: 70,
-                margin: const EdgeInsets.all(20),
-                child: ElevatedButton(
-                  onPressed: (account.lv >= 6)
-                      ? () {
-                          setState(() {
-                            color3 = Colors.white.withOpacity(0.8);
-                          });
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ManHinhTraLoi(
-                                        id: 51,
-                                        point: 0,
-                                        soluongcau: 1,
-                                      )));
-                        }
-                      : null,
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(color3),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)),
-                      )),
-                  child: const Text(
-                    '6',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black87),
-                  ),
-                ),
-              ),
-            ],
-          );
-        } else {
-          return Container();
-        }
-      }),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          height: 70,
+          width: 70,
+          margin: const EdgeInsets.all(20),
+          child: ElevatedButton(
+            onPressed: i1
+                ? () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ManHinhTraLoi(
+                          id: 31,
+                          point: 0,
+                          soluongcau: 1,
+                        ),
+                      ),
+                    );
+                  }
+                : null,
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(color1),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                )),
+            child: const Text(
+              '4',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87),
+            ),
+          ),
+        ),
+        Container(
+          height: 70,
+          width: 70,
+          margin: const EdgeInsets.all(20),
+          child: ElevatedButton(
+            onPressed: i2
+                ? () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ManHinhTraLoi(
+                                  id: 41,
+                                  point: 0,
+                                  soluongcau: 1,
+                                )));
+                  }
+                : null,
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(color2),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                )),
+            child: const Text(
+              '5',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87),
+            ),
+          ),
+        ),
+        Container(
+          height: 70,
+          width: 70,
+          margin: const EdgeInsets.all(20),
+          child: ElevatedButton(
+            onPressed: i3
+                ? () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ManHinhTraLoi(
+                                  id: 51,
+                                  point: 0,
+                                  soluongcau: 1,
+                                )));
+                  }
+                : null,
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(color3),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                )),
+            child: const Text(
+              '6',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
